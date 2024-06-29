fc178b0a46e52721e374e345497c937f8b3dd103
Wu Ziban <2314245651@qq.com>
2024-06-30 00:45:40 +0800

    Update password.py
    Add new feature about the password generator function， and make code style more standardized.

--- PASSWORD RELATED/RandomPassword/password.py
+++ PASSWORD RELATED/RandomPassword/password.py
@@ -1,29 +1,74 @@
 import string
 import secrets
 
-# Define the set of characters to be used in the password
-CHARACTER_SET = string.ascii_letters + string.digits + string.punctuation
+from enum import Enum
 
-def generate_password(length):
-    """Generate a random password of the specified length."""
-    password = ''.join(secrets.choice(CHARACTER_SET) for i in range(length))
+
+# Define the set of characters to be used in the password, three strings means three modes.
+
+
+class CharcterSet(Enum):
+    ONLYNUMBER = string.digits
+    NOPUNCTUATION = string.ascii_letters + string.digits
+    ALLCHARCTER = string.ascii_letters + string.digits + string.punctuation
+
+
+# Define a list of CharcterSet , in order to quickly transfer mode code to corresponding character set.
+
+CharcterSetList = [
+    CharcterSet.ALLCHARCTER,
+    CharcterSet.ONLYNUMBER,
+    CharcterSet.NOPUNCTUATION,
+]
+
+# Define Prompt Enum class , in order to amend prompts more easily .
+
+
+class Prompt(Enum):
+    GetPasswordInt = "How many passwords do you want to generate? "
+    GetPasswordLength = "Enter the length of the password(s): "
+    GetGenerationMode = "Choose password generation mode: (1) Only Number (2) Don't use punctuation (0) All ascii character "
+    ValueErrorOccured = "Please enter a valid integer."
+    PasswordGenerated = "Generated passwords:"
+
+
+def generate_password(length: int, generation_mode_code: int = 0) -> str:
+    """
+    generate_password
+
+    Generate a random password of the specified length by the specified mode.
+
+    Args:
+        length (int): specify the length of password.
+        generation_mode_code (int): specify the generation mode of password: (1) Only Number (2) Don't use punctuation (0/default) All ascii character.
+
+    Returns:
+        str: a password string
+    """
+    password = "".join(
+        secrets.choice(CharcterSetList[generation_mode_code].value)
+        for i in range(length)
+    )
     return password
+
 
 def main():
     # Prompt the user for the number of passwords to generate and their length
     while True:
         try:
-            num_pass = int(input("How many passwords do you want to generate? "))
-            password_length = int(input("Enter the length of the password(s): "))
+            num_pass = int(input(Prompt.GetPasswordInt.value))
+            password_length = int(input(Prompt.GetPasswordLength.value))
+            generation_mode_code = int(input(Prompt.GetGenerationMode.value))
             break
         except ValueError:
-            print("Please enter a valid integer.")
-            continue 
+            print(Prompt.ValueErrorOccured.value)
+            continue
     # Generate the specified number of passwords and print them to the console
-    print("Generated passwords:")
+    print(Prompt.PasswordGenerated.value)
     for i in range(num_pass):
-        password = generate_password(password_length)
+        password = generate_password(password_length, generation_mode_code)
         print(f"{i+1}. {password}")
+
 
 if __name__ == "__main__":
     main()
